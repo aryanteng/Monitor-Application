6911304edec4a20aa72afa37502d9c3a301644f9
aryanteng <aryanteng892@gmail.com>
2023-04-20 23:21:28 +0530

    initialised sensors

--- app/src/main/java/com/example/monitorapplication/MainActivity.kt
+++ app/src/main/java/com/example/monitorapplication/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.monitorapplication
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private lateinit var magnetometer: Sensor
+
+    private val alpha = 0.8f
+    private var gravity: FloatArray = floatArrayOf(0f, 0f, 0f)
+    private var geomagnetic: FloatArray = floatArrayOf(0f, 0f, 0f)
+    private var azimuth = 0f
+    private var lastAzimuth = 0f
+    private var lastStepCount = 0
+    private var stepCount = 0
+    private var distance = 0f
+    private var strideLength = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val height = 170 // user's height in cm
+        strideLength = 0.415f * height // calculate stride length
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
     }
 }
