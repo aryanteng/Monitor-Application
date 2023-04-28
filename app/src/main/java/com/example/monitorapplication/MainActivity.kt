ce2fd5a91c97f9a924e103d89d4b1017f690bbb7
aryanteng <aryanteng892@gmail.com>
2023-04-28 19:11:58 +0530

    threshold changes

--- app/src/main/java/com/example/monitorapplication/MainActivity.kt
+++ app/src/main/java/com/example/monitorapplication/MainActivity.kt
@@ -214,7 +214,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val movingAverage = accelerationMagnitudeList.average()
 
         // Check if the acceleration magnitude is greater than the moving average multiplied by the threshold factor
-        if (accelerationMagnitude > movingAverage * peakThresholdFactor && currentTimestamp - lastStepTime > stepDebounceTime) {
+        if (accelerationMagnitude > movingAverage + peakThresholdFactor && currentTimestamp - lastStepTime > stepDebounceTime) {
             lastStepTime = currentTimestamp
             Log.i("LIST", accelerationMagnitudeList.toString())
             Log.i("AVERAGE", movingAverage.toString())
